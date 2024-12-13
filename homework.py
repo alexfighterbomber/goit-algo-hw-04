20905aeb984747cb89ea2d5f3ff114299c4433e0
alexfighterbomber <ssamsobi@gmail.com>
2024-12-13 22:31:39 +0200

    2

--- homework.py
+++ homework.py
@@ -1,7 +1,23 @@
-def total_salary(path):
-    total = 5000
-    average = total/3
+def total_salary(path) -> tuple:
+    total = 0
+    count = 0
+    try:
+        with open(path, 'r',encoding='utf-8') as fh:
+            lines = [el.strip() for el in fh.readlines() if el.strip()]
+    except FileNotFoundError:
+        return (0,0)
+    except IOError:
+        return (0,0)
+
+    for line in lines:
+        developer = line.split(',')
+        if len(developer) == 2:
+            total += int(developer[1])
+            count += 1
+    
+    average = total/count
     return (total, average)
 
-total, average = total_salary("path/to/salary_file.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average:.2f}")
+
+total, average = total_salary("salary_file.txt")
+print(f"Загальна сума заробітної плати: {total:.2f}, Середня заробітна плата: {average:.2f}")
