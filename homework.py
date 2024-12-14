080964b62403ddd37a6ce1f719862f2645917bd0
alexfighterbomber <ssamsobi@gmail.com>
2024-12-14 14:40:41 +0200

    task2

--- homework.py
+++ homework.py
@@ -3,22 +3,41 @@ def total_salary(path: str) -> tuple:
     total = 0
     count = 0
     try:
-        with open(path, 'r',encoding='utf-8') as fh:
-            lines = [el.strip() for el in fh.readlines() if el.strip()]
+        with open(path, 'r',encoding='utf-8') as fh:    # відкриття файлу для читання
+            lines = [el.strip() for el in fh.readlines() if el.strip()] # отримання списку рядків з очищенням від пробільних символів та пустих рядків
     except FileNotFoundError:
-        return (0,0)
+        return (None,None)
     except IOError:
-        return (0,0)
+        return (None,None)
 
     for line in lines:
-        developer = line.split(',')
-        if len(developer) == 2 and developer[1].strip().isdigit():
-            total += float(developer[1])
+        developer = line.split(',')     # розділення на елементи
+        if len(developer) == 2 and developer[1].strip().isdigit():  # перевірка на коректність даних
+            total += float(developer[1])        # підрахунок total
             count += 1
     
-    average = total/count
+    average = total/count    # підрахунок average
     return (total, average)
+
+def get_cats_info(path):
+    try:
+        with open(path, 'r',encoding='utf-8') as fh:    # відкриття файлу для читання
+            lines = [el.strip() for el in fh.readlines() if el.strip()] # отримання списку рядків з очищенням від пробільних символів та пустих рядків
+    except FileNotFoundError:
+        return (None,None)
+    except IOError:
+        return (None,None)
+    cats = list()
+    for line in lines:
+        cat = line.split(',')    # розділення на елементи
+        if len(cat)==3: cats.append({"id":cat[0], "name":cat[1], "age":cat[2]}) # створення словників та заповнення списку 
+    return cats
+
+
 
 
 total, average = total_salary("salary_file.txt")
-print(f"Загальна сума заробітної плати: {total:.2f}, Середня заробітна плата: {average:.2f}")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+cats_info = get_cats_info("cats_file.txt")
+print(cats_info)
