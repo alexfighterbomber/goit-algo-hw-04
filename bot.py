3b68d68e53d4722f38433cf0c7d335ec5491bddb
alexfighterbomber <ssamsobi@gmail.com>
2024-12-15 12:23:14 +0200

    final

--- bot.py
+++ bot.py
@@ -8,14 +8,22 @@ def add_contact(args, contacts):
     contacts[name] = phone
     return "Contact added."
 
-def change_phone(args, contacts):
-    pass
+def change_contact(args, contacts):
+    name, phone = args # Считываем первые два элемент из списка args
+    if name in contacts:  # если имя есть в контактах
+        contacts[name] = phone
+        return "Contact updated." 
+    else: return 'Нема такого!'
 
-def get_phone(args, contacts):
-    pass
-
-def get_all(contacts):
-    pass
+def show_phone(args, contacts):
+    name = args[0]  # Считываем первый элемент из списка args
+    return contacts.get(name, "Такий тут не живе!")  # Возвращаем значение по ключу или ошибку если ключ не найден
+    
+def show_all(contacts):
+    ret_str = ""
+    for name in contacts:
+        ret_str += name + ': ' + contacts[name] + '\n'  # формируем строку для вывода контактов
+    return ret_str.rstrip() # обрезаем последний перевод строки
 
 def main():
     contacts = {}
@@ -24,20 +32,20 @@ def main():
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
+        if command in ["close", "exit"]:# команда "close", "exit"
             print("Good bye!")
             break
-        elif command == "hello":
+        elif command == "hello":        # команда "hello"
             print("How can I help you?")
-        elif command == "add":
+        elif command == "add":          # команда "add"
             print(add_contact(args, contacts))
-        elif command == 'change':
-            print(change_phone(args, contacts))
-        elif command == 'phone':
-            print(get_phone(args, contacts))
-        elif command == 'all':
-            print(get_all(contacts))
-        else:
+        elif command == 'change':       # команда 'change'
+            print(change_contact(args, contacts))
+        elif command == 'phone':        # команда 'phone'
+            print(show_phone(args, contacts))
+        elif command == 'all':          # команда 'all'
+            print(show_all(contacts))
+        else:                           # неизвестная  команда
             print("Invalid command.")
 
 if __name__ == "__main__":
